7de3b5b9d11ad9e247bb0a0dedf4873f334b4359
Leckly <minejoyjoy@gmail.com>
2023-06-13 16:52:01 +0200

    Spoty

---
+++ PoKiegoGrzybaAPI/Controllers/MushroomSpotsController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
+using PoKiegoGrzybaAPI.Data;
+using PoKiegoGrzybaAPI.Data.Req;
+using PoKiegoGrzybaAPI.Data.Response;
+using PoKiegoGrzybaAPI.Models;
+
+namespace PoKiegoGrzybaAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MushroomSpotsController : ControllerBase
+    {
+        private readonly PoKiegoGrzybaDbContext context;
+        public ILogger<MushroomSpotsController> _logger;
+
+        public MushroomSpotsController(PoKiegoGrzybaDbContext context, ILogger<MushroomSpotsController> logger)
+        {
+            this.context = context;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSpot([FromBody] MushroomSpotAdd mushroomSpotAdd)
+        {
+            try
+            {
+                var item = new MushroomSpot()
+                {
+                    Description = mushroomSpotAdd.Description,
+                    Latitude = mushroomSpotAdd.Latitude,
+                    Longitude = mushroomSpotAdd.Longitude,
+                    MushroomHunterId = mushroomSpotAdd.UserID,
+                    SpotName = mushroomSpotAdd.SpotName
+                };
+                await context.AddAsync(item);
+                await context.SaveChangesAsync();
+            }
+            catch(Exception ex) 
+            {
+                return Conflict(ex);
+            }
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUserSpots([FromQuery] long id)
+        {
+            try
+            {
+                var items = context.MushroomSpot.Where(x => x.MushroomHunterId == id).ToList();
+                return Ok(items);
+            }
+            catch(Exception ex)
+            {
+                return Conflict(ex);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSpot([FromBody] MushroomSpotUpdate mushroomSpotUpdate)
+        {
+            try
+            {
+                var item = context.MushroomSpot.FirstOrDefault(x => x.Id == mushroomSpotUpdate.ID);
+                if (item != null)
+                {
+                    item.Longitude = mushroomSpotUpdate.Longitude;
+                    item.Latitude = mushroomSpotUpdate.Latitude;
+                    item.SpotName = mushroomSpotUpdate.SpotName;
+                    item.Description = mushroomSpotUpdate.Description;
+                    await context.SaveChangesAsync();
+                }
+                else
+                {
+                    return NotFound(new ErrorMessage()
+                    {
+                        Message = "There is no spot with that Id"
+                    });
+                }
+            }catch(Exception ex)
+            {
+                return Conflict(ex);
+            }
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSpot([FromQuery] long id)
+        {
+            try
+            {
+                var item = context.MushroomSpot.FirstOrDefault(x => x.Id == id);
+                context.MushroomSpot.Remove(item);
+                await context.SaveChangesAsync();
+            }
+            catch(Exception ex)
+            {
+                return Conflict(ex);
+            }
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetSpots()
+        {
+            return Ok(context.MushroomSpot.ToList());
+        }
+    }
+}
---
+++ PoKiegoGrzybaAPI/Data/Req/MushroomSpotAdd.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PoKiegoGrzybaAPI.Data.Req
+{
+    public class MushroomSpotAdd
+    {
+        [Required]
+        public string SpotName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Latitude { get; set; }
+        [Required]
+        public double Longitude { get; set; }
+        [Required]
+        public long UserID { get; set; }
+    }
+}
